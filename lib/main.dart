71f81435f5529bff0150fa7a403a57da296b3737
Dheeraj <dheerajraina55@gmail.com>
2022-09-25 19:56:12 +0530

    Adding firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'dart:developer';
 
-
-
 import 'package:acme_health_tracker_assignment/basewidget/base_widget.dart';
+import 'package:acme_health_tracker_assignment/pages/recordspage/records_page.dart';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
-void main() {
+
+Future <void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ACMEHealth());
 }
 
@@ -16,18 +23,16 @@ class ACMEHealth extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      getPages: [
+        GetPage(name: '/allRecords/:index', page: (() => RecordsPage(indexClicked:int.parse(Get.parameters['index'].toString())))),
+      ],
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.grey,
-       ),
+      ),
       home: const BaseWidget(title: 'ACME Health'),
-      
     );
   }
 }
-
-
-
-
