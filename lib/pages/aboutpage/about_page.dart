1995190e1774cc05e4fe6a79528a15de70c7c581
Dheeraj <dheerajraina55@gmail.com>
2022-09-25 19:57:09 +0530

    About page for writing the company bio

---
+++ lib/pages/aboutpage/about_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("About Page"),),
+    );
+  }
+}
