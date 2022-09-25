76cd0c43de3a1d9b419602beba0912ead693745f
Dheeraj <dheerajraina55@gmail.com>
2022-09-25 11:35:29 +0530

    Home Page Initial commit

--- lib/homepage/home_page.dart
+++ lib/homepage/home_page.dart
@@ -1,14 +1,13 @@
 import 'dart:developer';
-
-
+import 'package:acme_health_tracker_assignment/homepage/widgets/tracker_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
+  const HomePage({Key? key, }) : super(key: key);
 
-  final String title;
+  
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -20,38 +19,7 @@ class _HomePageState extends State<HomePage> {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: RichText(
-            text: TextSpan(children: [
-          TextSpan(
-            text: widget.title.split(" ")[0],
-            style: GoogleFonts.coveredByYourGrace(
-                color: Colors.black, fontSize: screenWidth / 18),
-          ),
-          TextSpan(
-            text: " ",
-            style: GoogleFonts.acme(
-              color: Colors.black,
-            ),
-          ),
-          TextSpan(
-            text: widget.title.split(" ")[1],
-            style: GoogleFonts.openSans(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: screenWidth / 18),
-          )
-        ])),
-      ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(CupertinoIcons.app), label: "About Us"),
-      ],
-      onTap: ((value) => log("${value}")),
-      ),
-    ));
+        child: Scaffold(body: Center(child: Text("Home Page"),),)
+    );
   }
 }
