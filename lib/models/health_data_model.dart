d57c349c0315ed42367457a9be2e3ee04969e4ae
Dheeraj <dheerajraina55@gmail.com>
2022-09-26 12:23:45 +0530

    adding id field into health data model

--- lib/models/health_data_model.dart
+++ lib/models/health_data_model.dart
@@ -5,16 +5,20 @@ List<UserDataModel> userFromJson(String str) => List<UserDataModel>.from(json.de
 
 class UserDataModel{
   UserDataModel({
+    required this.id,
     required this.type,
     required this.value,
     required this.createdOn,
   });
+
+  String id;
 
   String type;
   String value;
   DateTime createdOn;
   
 factory UserDataModel.fromJson(Map<String, dynamic> json) => UserDataModel(
+        id: json["id"],
         type: json["type"],
         value: json["value"],
         createdOn: json["createdOn"].toDate(),
