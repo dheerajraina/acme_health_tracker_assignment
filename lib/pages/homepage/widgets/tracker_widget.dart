cf9375fcdad9e4780700881ba2d8a099b42acaa5
Dheeraj <dheerajraina55@gmail.com>
2022-09-26 12:25:55 +0530

    Adding an button to tracker widget for adding new data

--- lib/pages/homepage/widgets/tracker_widget.dart
+++ lib/pages/homepage/widgets/tracker_widget.dart
@@ -1,40 +1,53 @@
 import 'dart:developer';
 
+import 'package:acme_health_tracker_assignment/network/firebase_operations.dart';
 import 'package:acme_health_tracker_assignment/pages/homepage/controllers/tracker_widget_controller.dart';
+import 'package:acme_health_tracker_assignment/pages/homepage/home_page.dart';
+import 'package:acme_health_tracker_assignment/pages/recordspage/records_page_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class TrackerWidgetBuilder extends StatefulWidget {
-  TrackerWidgetBuilder({Key? key, required this.title, required this.index,this.intialValue=0});
+  TrackerWidgetBuilder(
+      {Key? key,
+      required this.title,
+      required this.index,
+      this.intialValue = "0"});
 
   String title;
   int index;
-  int intialValue ;
+  String intialValue;
 
   @override
   State<TrackerWidgetBuilder> createState() => _TrackerWidgetBuilderState();
 }
 
 class _TrackerWidgetBuilderState extends State<TrackerWidgetBuilder> {
+  final TrackerWidgetController _trackerWidgetController =
+      Get.put(TrackerWidgetController());
 
+  final FirebaseOperations _firebaseOperations=new FirebaseOperations();
 
-final TrackerWidgetController _trackerWidgetController=Get.put(TrackerWidgetController());
 
   @override
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
 
-    return Card(
+    return 
+    // Obx((){return
+     Card(
         elevation: 10,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         child: Container(
           child: ListTile(
             onTap: () {
               log("${widget.index}");
-              Get.toNamed("/allRecords/${widget.index}",);
+              Get.toNamed(
+                "/allRecords/${widget.index}",
+              );
             },
             contentPadding: EdgeInsets.symmetric(
                 horizontal: screenWidth / 20, vertical: screenHeight / 50),
@@ -45,31 +58,60 @@ final TrackerWidgetController _trackerWidgetController=Get.put(TrackerWidgetCont
             trailing: InkWell(
               onTap: () {
                 log("Add ${widget.index}");
-                showDialog(context: context, builder: ((context) {
-                  return SimpleDialog(
-                    
-                    title: Text(
-                      "Add New ${widget.title}",
-                      style: GoogleFonts.nanumGothic(),
-                      ),
-                      children: [
-                        Container(
+                showDialog(
+                    context: context,
+                    builder: ((context) {
+                      var value;
+                      return SimpleDialog(
+                        title: Text(
+                          "Add New ${widget.title}",
+                          style: GoogleFonts.nanumGothic(),
+                        ),
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(left: 10, right: 10),
+                              child: TextField(
+                                onChanged: (val){
+                                  value=val;
+                                },
+                                keyboardType: TextInputType.number,
+                              )),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          CupertinoButton(
+                              color: Colors.grey.shade300,
+                              child: Text("Add"),
+                              onPressed: () {
+                                var data ={
+                                  "type":widget.index==0?"weight":widget.index==1?"blood_pressure":"exercise",
+                                  "value":value,
+                                  "createdOn":DateTime.now(),
+                                };
+                                widget.index==0?_trackerWidgetController.latestWeight.value=value:_trackerWidgetController.latestBP.value=value;
+                               var response= _firebaseOperations.storeData(data);
+                               log("Response = $response");
+                               
+                                _firebaseOperations.fetchData();
+                                
+                                 Navigator.pop(context);
 
-                          margin: EdgeInsets.only(left: 10,right: 10),
-                          child: TextField(
-                            keyboardType: TextInputType.number,
-                          )),
-                          SizedBox(height: 8,),
-                          CupertinoButton(color: Colors.grey.shade300,child: Text("Add"), onPressed: (){
+                                
+                                
 
-                          }),
-                          SizedBox(height: 5,),
-                          CupertinoButton(color: Colors.grey.shade300,child: Text("Cancel"), onPressed: (){Navigator.pop(context);}),
-                          
-                      ],
+                              }),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          CupertinoButton(
+                              color: Colors.grey.shade300,
+                              child: Text("Cancel"),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              }),
+                        ],
                       );
-
-                }));
+                    }));
               },
               child: Container(
                   child: Center(child: Text("Add")),
@@ -81,12 +123,14 @@ final TrackerWidgetController _trackerWidgetController=Get.put(TrackerWidgetCont
                         width: 1,
                       ))),
             ),
-            subtitle: widget.index==0
-                      ?Text("${_trackerWidgetController.latestWeight.value} Kgs")
-                      :widget.index==1
-                      ?Text("${_trackerWidgetController.latestBP.value} mmHh")
-                      :Text("${_trackerWidgetController.latestExercise.value} min"),
+            subtitle: widget.index == 0
+                ? Text(" Kgs")
+                : widget.index == 1
+                    ? Text(" mmHh")
+                    : Text(
+                        " minutes"),
           ),
         ));
+    // });
   }
 }
