7d3e5f4b4c83c7022e04c3cc28a75f7979f04daa
Dheeraj <dheerajraina55@gmail.com>
2022-09-26 12:30:14 +0530

    Adding a button to enter new records from thr records page

--- lib/pages/recordspage/records_page.dart
+++ lib/pages/recordspage/records_page.dart
@@ -1,72 +1,169 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:acme_health_tracker_assignment/models/health_data_model.dart';
 import 'package:acme_health_tracker_assignment/network/firebase_operations.dart';
+import 'package:acme_health_tracker_assignment/pages/homepage/controllers/tracker_widget_controller.dart';
 import 'package:acme_health_tracker_assignment/pages/recordspage/records_page_controller.dart';
 import 'package:acme_health_tracker_assignment/pages/recordspage/widgets/record_widget.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class RecordsPage extends StatefulWidget {
-   RecordsPage({Key? key,required this.indexClicked});
+  RecordsPage({
+    Key? key,
+    required this.indexClicked,
+  });
 
-  int indexClicked=0;
+  int indexClicked = 0;
 
-  final RecordsPageController _recordsPageController=Get.put(RecordsPageController());
+  final RecordsPageController _recordsPageController =
+      Get.put(RecordsPageController());
 
   @override
   State<RecordsPage> createState() => _RecordsPageState();
 }
 
 class _RecordsPageState extends State<RecordsPage> {
-
- 
-  final RecordsPageController _recordsPageController =Get.put(RecordsPageController());
-   
-   StreamController<List<dynamic>>? _streamController=new StreamController();
+  final RecordsPageController _recordsPageController =
+      Get.put(RecordsPageController());
 
   @override
   void initState() {
     // TODO: implement initState
-    _streamController=StreamController();
-     super.initState();
+
+    super.initState();
   }
 
-  
   @override
   Widget build(BuildContext context) {
-     var screenHeight = MediaQuery.of(context).size.height;
+    FirebaseOperations()
+        .analytics
+        .setCurrentScreen(screenName: "Records Page => ${widget.indexClicked}");
+    var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
 
-    log("---- Data  =  ${_recordsPageController.weightRecords.value},index= ${widget.indexClicked}");
-    var records=widget.indexClicked==0
-                ?_recordsPageController.weightRecords.value
-                :widget.indexClicked==1
-                ?_recordsPageController.bloodPressureRecords.value
-                :_recordsPageController.exerciseRecords.value;
+    log("---- Data  =  ${_recordsPageController.records[widget.indexClicked]},index= ${widget.indexClicked}");
+    // var records = widget.indexClicked == 0
+    //     ? _recordsPageController.weightRecords.value
+    //     : widget.indexClicked == 1
+    //         ? _recordsPageController.bloodPressureRecords.value
+    //         : _recordsPageController.exerciseRecords.value;
 
-    
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.white,),
-      body: Container(
-        child:Builder(builder: (context){
-          return records.length>0 
-            ? ListView.builder(
-              itemCount: records.length,
-              itemBuilder: (context,index){
-              return RecordWidget(value:records[index].value, createdOn: records[index].createdOn, index: widget.indexClicked);
-            })
-            :Center(child: Container(
-              child: Text(
-                "No Records Available",
-                style: GoogleFonts.nanumGothic(fontSize: screenWidth/20,fontWeight: FontWeight.bold,),
-                ),));
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            "${widget.indexClicked == 0 ? "Weight" : widget.indexClicked == 1 ? "Blood Pressure" : "Exercise"}",
+            style: GoogleFonts.lato(),
+          ),
+          actions: [
+            InkWell(
+              onTap: () {
+                log("Add ${widget.indexClicked}");
+                showDialog(
+                    context: context,
+                    builder: ((context) {
+                      var value;
+                      return SimpleDialog(
+                        title: Text(
+                          "Add New ${Get.find<TrackerWidgetController>().trackers[widget.indexClicked]}",
+                          style: GoogleFonts.nanumGothic(),
+                        ),
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(left: 10, right: 10),
+                              child: TextField(
+                                onChanged: (val) {
+                                  value = val;
+                                },
+                                keyboardType: TextInputType.number,
+                              )),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          CupertinoButton(
+                              color: Colors.grey.shade300,
+                              child: Text("Add"),
+                              onPressed: () {
+                                var data = {
+                                  "type": widget.indexClicked == 0
+                                      ? "weight"
+                                      : widget.indexClicked == 1
+                                          ? "blood_pressure"
+                                          : "exercise",
+                                  "value": value,
+                                  "createdOn": DateTime.now(),
+                                };
+                                widget.indexClicked == 0
+                                    ? Get.find<TrackerWidgetController>()
+                                        .latestWeight
+                                        .value = value
+                                    : Get.find<TrackerWidgetController>()
+                                        .latestBP
+                                        .value = value;
+                                var response =
+                                    FirebaseOperations().storeData(data);
+                                log("Response = $response");
 
-        }),
-        )
+                                FirebaseOperations().fetchData();
 
-        
-    );
+                                Future.delayed(Duration(seconds: 2), () {
+                                  setState(() {});
+                                  Navigator.pop(context);
+                                });
+                              }),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          CupertinoButton(
+                              color: Colors.grey.shade300,
+                              child: Text("Cancel"),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              }),
+                        ],
+                      );
+                    }));
+              },
+              child: Padding(
+                padding: EdgeInsets.only(right: screenWidth / 20),
+                child: Center(child: Text("Add")),
+              ),
+            )
+          ],
+        ),
+        body: Container(
+          child: Builder(builder: (context) {
+            return _recordsPageController
+                    .records[widget.indexClicked].isNotEmpty
+                ? ListView.builder(
+                    itemCount: _recordsPageController
+                        .records[widget.indexClicked].length,
+                    itemBuilder: (context, index) {
+                      var records =
+                          _recordsPageController.records[widget.indexClicked];
+                      return RecordWidget(
+                        value: records[index].value,
+                        createdOn: records[index].createdOn,
+                        trackerIndex: widget.indexClicked,
+                        index: index,
+                      );
+                    })
+                : Center(
+                    child: Container(
+                    child: Text(
+                      "No Records Available",
+                      style: GoogleFonts.nanumGothic(
+                        fontSize: screenWidth / 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ));
+          }),
+        ));
   }
 }
