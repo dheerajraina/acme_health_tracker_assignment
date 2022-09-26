df2c44dc6d5db16212378d408709b8948de55c84
Dheeraj <dheerajraina55@gmail.com>
2022-09-26 12:29:30 +0530

    Adding firebase analytics support to the app along with adding updating the routes to records page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:acme_health_tracker_assignment/basewidget/base_widget.dart';
+import 'package:acme_health_tracker_assignment/pages/homepage/home_page.dart';
 import 'package:acme_health_tracker_assignment/pages/recordspage/records_page.dart';
 
 import 'package:flutter/material.dart';
@@ -8,6 +9,7 @@ import 'package:get/get.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import'package:firebase_analytics/firebase_analytics.dart';
 
 
 
@@ -23,16 +25,26 @@ class ACMEHealth extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+     FirebaseAnalytics analytics=FirebaseAnalytics.instance;
+     FirebaseAnalyticsObserver observer=FirebaseAnalyticsObserver(analytics: analytics);
+
+     analytics.setUserId(id: 'QwkJSRRjFu9PWncnQxhb');
+     
+
     return GetMaterialApp(
+      navigatorObservers:<NavigatorObserver> [observer],
+      
       getPages: [
-        GetPage(name: '/allRecords/:index', page: (() => RecordsPage(indexClicked:int.parse(Get.parameters['index'].toString())))),
+        GetPage(title:"RecordsPage",name: '/allRecords/:index', page: (() => RecordsPage(indexClicked:int.parse(Get.parameters['index'].toString())))),
+        GetPage(title:"HomePage",name: '/', page: (() => HomePage())),
       ],
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      home: const BaseWidget(title: 'ACME Health'),
+      home: BaseWidget(title: 'ACME Health',observer: observer,analytics: analytics,),
     );
   }
 }
