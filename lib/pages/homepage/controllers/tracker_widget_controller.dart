6a229515f643343ba269067735cd2f9b91098256
Dheeraj <dheerajraina55@gmail.com>
2022-09-26 12:24:55 +0530

    Tracker widget controller update
    Adding a list of trackers

---
+++ lib/pages/homepage/controllers/tracker_widget_controller.dart
@@ -0,0 +1,9 @@
+import 'package:acme_health_tracker_assignment/pages/recordspage/records_page_controller.dart';
+import 'package:get/get.dart';
+
+class TrackerWidgetController extends GetxController{
+
+  var trackers=["Weight (in Kgs)","Blood Pressure (in mmHg)","Daily Exercise (in minutes)"].obs;
+ 
+  }
+  
